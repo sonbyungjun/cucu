d64549250a552b26e49508b991520fc1e98ff003
Byungjun Son <saint2030@naver.com>
2020-02-12 16:54:49 +0900

    images

--- lib/default/src/passport/passport.index.ts
+++
@@ -1,8 +0,0 @@
-import local from './localStrategy';
-import jwt from './jwtStrategy';
-import passport from 'passport';
-
-export default (passport: passport.PassportStatic) => {
-  local(passport);
-  jwt(passport);
-};
